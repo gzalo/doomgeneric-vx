d0639715d0f282321aa009fcc9a11347e59ca780
Gonzalo Avila Alterach <gonzaloavilaalterach@gmail.com>
2024-11-30 02:03:20 -0300

    Improve performance

--- doomgeneric/i_video.c
+++ doomgeneric/i_video.c
@@ -157,12 +157,9 @@ void cmap_to_fb(uint8_t * out, uint8_t * in, int in_pixels)
     for (i = 0; i < in_pixels; i++)
     {
         c = colors[*in];  /* R:8 G:8 B:8 format! */
-        r = (uint16_t)(c.r >> (8 - s_Fb.red.length));
-        g = (uint16_t)(c.g >> (8 - s_Fb.green.length));
-        b = (uint16_t)(c.b >> (8 - s_Fb.blue.length));
-        pix = r << s_Fb.red.offset;
-        pix |= g << s_Fb.green.offset;
-        pix |= b << s_Fb.blue.offset;
+        pix = c.r;
+        pix |= c.g << 8;
+        pix |= c.b << 16;
 
         for (k = 0; k < fb_scaling; k++) {
             for (j = 0; j < s_Fb.bits_per_pixel/8; j++) {
