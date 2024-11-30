c91ab5fbb4a9d7cb1c4445b7c03cf885ed03b9b9
Gonzalo Avila Alterach <gonzaloavilaalterach@gmail.com>
2024-11-30 00:03:06 -0300

    Fix framebuffer

--- doomgeneric/i_video.c
+++ doomgeneric/i_video.c
@@ -183,17 +183,17 @@ void I_InitGraphics (void)
 	s_Fb.yres = DOOMGENERIC_RESY;
 	s_Fb.xres_virtual = s_Fb.xres;
 	s_Fb.yres_virtual = s_Fb.yres;
-	s_Fb.bits_per_pixel = 32;
+	s_Fb.bits_per_pixel = 24;
 
 	s_Fb.blue.length = 8;
 	s_Fb.green.length = 8;
 	s_Fb.red.length = 8;
-	s_Fb.transp.length = 8;
+	s_Fb.transp.length = 0;
 
 	s_Fb.blue.offset = 0;
 	s_Fb.green.offset = 8;
 	s_Fb.red.offset = 16;
-	s_Fb.transp.offset = 24;
+	s_Fb.transp.offset = 0;
 	
 
     printf("I_InitGraphics: framebuffer: x_res: %d, y_res: %d, x_virtual: %d, y_virtual: %d, bpp: %d\n",
