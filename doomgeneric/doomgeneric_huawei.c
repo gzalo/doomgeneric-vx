93f3317603d1657e2fcc1638adf4e92aea863320
Gonzalo Avila Alterach <gonzaloavilaalterach@gmail.com>
2024-11-30 00:55:17 -0300

    Add basic input handling

--- doomgeneric/doomgeneric_huawei.c
+++ doomgeneric/doomgeneric_huawei.c
@@ -14,11 +14,13 @@
 #include <sys/mman.h>
 #include <sys/time.h>
 #include <linux/fb.h>
+#include <linux/input.h>
 
 static int frameBufferFd = -1;
 static uint8_t* frameBuffer = 0;
 const int screenWidth = 800;
 const int screenHeight = 480;
+static int eventFd;
 
 void DG_Init()
 {
@@ -35,6 +37,15 @@ void DG_Init()
         printf("FrameBuffer mmap failed\n");
         exit(1);
     }
+
+    eventFd = open("/dev/input/event0", O_RDONLY);
+    if (fd == -1) {
+        perror("Cannot open input device");
+        exit(1);
+    }
+
+    int flags = fcntl(eventFd, F_GETFL, 0);
+    fcntl(eventFd, F_SETFL, flags | O_NONBLOCK);
 }
 
 void DG_DrawFrame()
@@ -59,6 +70,21 @@ uint32_t DG_GetTicksMs()
 
 int DG_GetKey(int* pressed, unsigned char* doomKey)
 {
+    ssize_t bytes = read(eventFd, &ev, sizeof(struct input_event));
+    struct input_event ev;
+    
+    if (bytes == sizeof(struct input_event)) {
+        if (ev.type == EV_KEY) {
+            if(ev.code == 12) doomKey = KEY_FIRE, pressed = ev.value, return 1;
+            if(ev.code == 15) doomKey = KEY_USE, pressed = ev.value, return 1;
+            if(ev.code == 28) doomKey = KEY_ESCAPE, pressed = ev.value, return 1;
+            if(ev.code == 20) doomKey = KEY_ENTER, pressed = ev.value, return 1;
+            if(ev.code == 16) doomKey = KEY_UP, pressed = ev.value, return 1;
+            if(ev.code == 17) doomKey = KEY_DOWN, pressed = ev.value, return 1;
+            if(ev.code == 18) doomKey = KEY_LEFT, pressed = ev.value, return 1;
+            if(ev.code == 19) doomKey = KEY_RIGHT, pressed = ev.value, return 1;
+        }
+    } 
     return 0;
 }   
 
